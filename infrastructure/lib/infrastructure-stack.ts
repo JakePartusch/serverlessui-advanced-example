b175a452cccf53bac2158e2a4e32a336602e34fc
Jake Partusch <jakepartusch@gmail.com>
2021-02-16 11:45:15 -0600

    feat: adding parameters for custom domain deploy

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from '@aws-cdk/core';
 import { ServerlessUI } from '@serverlessui/construct';
 import { Source } from '@aws-cdk/aws-s3-deployment';
+import { Certificate } from '@aws-cdk/aws-certificatemanager';
+import { HostedZone } from '@aws-cdk/aws-route53';
 import { Table, AttributeType, ProjectionType } from '@aws-cdk/aws-dynamodb';
 import { Order } from '../../types/generated/graphql-resolvers';
 
@@ -30,10 +32,23 @@ export class InfrastructureStack extends cdk.Stack {
     });
 
     const { functions } = new ServerlessUI(this, 'ServerlessUI', {
+      buildId: 'advanced-example',
       uiSources: [Source.asset(`${__dirname}/../../build`)],
       apiEntries: [`${__dirname}/../../functions/graphql.ts`],
       apiEnvironment: {
         TABLE_NAME: table.tableName,
+      },
+      domain: {
+        domainName: 'serverlessui.app',
+        hostedZone: HostedZone.fromHostedZoneAttributes(this, 'HostedZone', {
+          hostedZoneId: 'Z10061011Y616GGTRN1OW',
+          zoneName: 'serverlessui.app',
+        }),
+        certificate: Certificate.fromCertificateArn(
+          this,
+          'Certificate',
+          'arn:aws:acm:us-east-1:857786057494:certificate/be831128-bb68-4fcd-b903-4d112d8fd2cd',
+        ),
       },
     });
 
