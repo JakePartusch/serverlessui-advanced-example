d33875ecf7ab564c17af294635c870a0cbc6fd07
Jake Partusch <jakepartusch@gmail.com>
2021-02-21 20:40:39 -0600

    feat: refactoring complete for demo

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -29,7 +29,7 @@ export class InfrastructureStack extends cdk.Stack {
       'status',
       'totalPrice',
       'createdDate',
-      'customerFullName',
+      'customer',
     ];
 
     table.addGlobalSecondaryIndex({
